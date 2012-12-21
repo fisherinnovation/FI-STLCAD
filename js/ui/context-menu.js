b32b92dd10eab098c300968039006def646140a6
Matt Fisher <fisher.matt@gmail.com>
2012-12-21 01:08:18 -0500

    Added dynamic context menu items and addition features to the context menu.

--- js/ui/context-menu.js
+++ js/ui/context-menu.js
@@ -1,5 +1,14 @@
 ContextMenu = function(thingiview) {
 	
+	var menuItems = [
+						'Undo',
+						'Copy',
+						'Paste',
+						'Delete',
+						'Level on Platform',
+						'Center on Platform'
+					]; 
+	
 	var _this = this;
 	var inited = false;
 	
@@ -11,17 +20,50 @@ ContextMenu = function(thingiview) {
 	    
 	    if(!inited) {
 	    	$('#context-menu .delete').bind('click', _this.onContextDeleteClick);
+	    	$('#context-menu .levelonplatform').bind('click', onContextLevelOnPlatformClick);
+	    	$('#context-menu .centeronplatform').bind('click', onContextCenterOnPlatformClick);
+	    	
 	    	inited = true;
 	    }
 	}).bind("click", function(event) {
 	    $("#context-menu").fadeOut(100);
 	});
 	
+	init();
+	
+	/**
+	 * ContextMenu Constructor
+	 */
+	function init() {
+		// Append items to menu
+		var menuHTML = '';
+		var l = menuItems.length;
+		for(var i = 0; i < l; i++) {
+			menuHTML += '<li><a href="#" class="' + menuItems[i].replace(/\s/g, '').toLowerCase() + '">' + menuItems[i] + '</a></li>';
+		}
+		$('#context-menu ul').html(menuHTML);
+	}
+	
 	
 	/**
 	 * 
 	 */
-	this.onContextDeleteClick = function() {
+	function onContextLevelOnPlatformClick(e) {
+		thingiview.levelObject();
+	}
+	
+	
+	/**
+	 * 
+	 */
+	function onContextCenterOnPlatformClick(e) {
+		thingiview.centerModel();
+	}
+	
+	/**
+	 * 
+	 */
+	function onContextDeleteClick(e) {
 		thingiview.removeSelectedObject();
 	}
 }
